0777e824b5ebab170efca6d5fba4f00044c299cc
sebastianochiari <sebastiano.chiari@studenti.unitn.it>
2018-09-26 15:01:28 +0200

    import local develop part

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.partners-carousel').slick( {
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        centerMode: true,
+        variableWidth: true
+    });
+});
